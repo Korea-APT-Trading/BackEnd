387eae58df3a8aa589dcbd451f1e325a289153b4
junuu <bababrll@naver.com>
2022-05-23 11:06:46 +0900

    #5 MemberMapper 수정 : 회원 CRUD 메서드 구현

--- src/main/java/com/ssafy/vue/mapper/MemberMapper.java
+++ src/main/java/com/ssafy/vue/mapper/MemberMapper.java
@@ -3,11 +3,14 @@ package com.ssafy.vue.mapper;
 import java.sql.SQLException;
 
 import com.ssafy.vue.dto.MemberDto;
+import org.apache.ibatis.jdbc.SQL;
 
 
 public interface MemberMapper {
 
 	public MemberDto login(MemberDto memberDto) throws SQLException;
-	public MemberDto userInfo(String userid) throws SQLException;
-	
+	public MemberDto selectInfo(String userId) throws SQLException;
+	public int updateInfo(MemberDto memberDto) throws SQLException;
+	public int deleteInfo(String userId) throws SQLException;
+	public int insertInfo(MemberDto memberDto) throws SQLException;
 }
