6764165141c78d01de555c92f0a2af73e32215d9
junuu <bababrll@naver.com>
2022-05-24 01:59:56 +0900

    #24 JwtServiceImplTest 생성 : 토큰을 통해 유효성 검사와 userId를 꺼내오는 테스트 수행

---
+++ src/test/java/com/ssafy/vue/service/JwtServiceImplTest.java
@@ -0,0 +1,62 @@
+package com.ssafy.vue.service;
+
+import com.ssafy.vue.dto.MemberDto;
+import com.ssafy.vue.mapper.MemberMapper;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class JwtServiceImplTest {
+
+    @Autowired
+    JwtService jwtService;
+
+    @Test
+    @DisplayName("토큰 발행 인증 테스트")
+    public void tokenAccessTest() throws Exception {
+        //given
+        MemberDto memberDto = makeMemberDto();
+
+        String token = jwtService.create("userid", memberDto.getUserid(), "access-token");// key, data, subject
+        if (jwtService.isUsable(token)) {
+            System.out.println(token);
+        } else {
+            fail();
+        }
+    }
+
+    @Test
+    @DisplayName("토큰에서 userId 꺼내오기 테스트")
+    public void getUserIdByToken() throws Exception {
+        //given
+        String SALT = "ssafySecret";
+        MemberDto memberDto = makeMemberDto();
+        String token = jwtService.create("userid", memberDto.getUserid(), "access-token");// key, data, subject
+
+        //when
+        Jws<Claims> claims = null;
+        claims = Jwts.parser()
+                     .setSigningKey(SALT.getBytes("UTF-8"))
+                     .parseClaimsJws(token);
+
+        Assertions.assertThat(claims.getBody().get("userid")).isEqualTo("junwoo");
+    }
+
+    public MemberDto makeMemberDto() {
+        MemberDto memberDto = new MemberDto();
+        memberDto.setUserid("junwoo");
+        memberDto.setUserpwd("1234");
+        memberDto.setUsername("jun");
+        memberDto.setEmail("bababrll@naver.com");
+        return memberDto;
+    }
+
+}
