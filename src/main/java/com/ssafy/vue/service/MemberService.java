16c878fe8addd89f06c31141a53351903db3c0ea
junuu <bababrll@naver.com>
2022-05-23 11:07:40 +0900

    #7 MemberService 메서드 추가  : 회원 CRUD 메서드 구현

--- src/main/java/com/ssafy/vue/service/MemberService.java
+++ src/main/java/com/ssafy/vue/service/MemberService.java
@@ -5,6 +5,10 @@ import com.ssafy.vue.dto.MemberDto;
 public interface MemberService {
 
 	public MemberDto login(MemberDto memberDto) throws Exception;
-	public MemberDto userInfo(String userid) throws Exception;
-	
+	public MemberDto selectInfo(String userid) throws Exception;
+	public int updateInfo(MemberDto memberDto) throws Exception;
+	public int deleteInfo(String userId) throws Exception;
+	public int insertInfo(MemberDto memberDto) throws Exception;
+	public MemberDto hasingPassword(MemberDto memberDto);
+	public boolean checkMemberIsDuplicate(String userId) throws Exception;
 }
--- src/main/java/com/ssafy/vue/service/MemberServiceImpl.java
+++ src/main/java/com/ssafy/vue/service/MemberServiceImpl.java
@@ -1,5 +1,6 @@
 package com.ssafy.vue.service;
 
+import org.mindrot.jbcrypt.BCrypt;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -9,20 +10,64 @@ import com.ssafy.vue.mapper.MemberMapper;
 @Service
 public class MemberServiceImpl implements MemberService {
 
-	@Autowired
-	private MemberMapper memberMapper;
-	
-	@Override
-	public MemberDto login(MemberDto memberDto) throws Exception {
-		if(memberDto.getUserid() == null || memberDto.getUserpwd() == null)
-			return null;
+    @Autowired
+    private MemberMapper memberMapper;
 
-		return memberMapper.login(memberDto);
-	}
 
-	@Override
-	public MemberDto userInfo(String userid) throws Exception {
-		return memberMapper.userInfo(userid);
-	}
+    @Override
+    public MemberDto login(MemberDto memberDto) throws Exception {
+        if (memberDto.getUserid() == null || memberDto.getUserpwd() == null)
+            return null;
+
+        MemberDto findById = memberMapper.selectInfo(memberDto.getUserid());
+        if(findById == null){
+            return null;
+        }
+
+        if(BCrypt.checkpw(memberDto.getUserpwd(),findById.getUserpwd())){
+            findById.setUserpwd(memberDto.getUserpwd());
+            return findById;
+        }
+
+        return null;
+    }
+
+    @Override
+    public MemberDto selectInfo(String userid) throws Exception {
+        return memberMapper.selectInfo(userid);
+    }
+
+    @Override
+    public int updateInfo(MemberDto memberDto) throws Exception {
+        return memberMapper.updateInfo(memberDto);
+    }
+
+    @Override
+    public int deleteInfo(String userId) throws Exception {
+        return memberMapper.deleteInfo(userId);
+    }
+
+    @Override
+    public int insertInfo(MemberDto memberDto) throws Exception {
+        memberDto = hasingPassword(memberDto);
+        return memberMapper.insertInfo(memberDto);
+    }
+
+    public MemberDto hasingPassword(MemberDto memberDto){
+        String userPassword = memberDto.getUserpwd();
+        String hashedPassword = BCrypt.hashpw(userPassword, BCrypt.gensalt());
+        memberDto.setUserpwd(hashedPassword);
+        return memberDto;
+    }
+
+    @Override
+    public boolean checkMemberIsDuplicate(String userId) throws Exception {
+        MemberDto findById = memberMapper.selectInfo(userId);
+        if(findById == null){
+            return false;
+        }
+        return true;
+    }
+
 
 }
