b1511e873981ba34ca555593501b79809a6d9fa4
junuu <bababrll@naver.com>
2022-05-24 14:36:13 +0900

    #30 BoardController 메서드 추가 : 게시물 댓글 기능 추가

--- src/main/java/com/ssafy/vue/controller/BoardController.java
+++ src/main/java/com/ssafy/vue/controller/BoardController.java
@@ -3,6 +3,8 @@ package com.ssafy.vue.controller;
 import java.io.UnsupportedEncodingException;
 import java.util.List;
 
+import com.ssafy.vue.dto.BoardComment;
+import com.ssafy.vue.service.BoardCommentService;
 import com.ssafy.vue.service.JwtService;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jws;
@@ -35,83 +37,102 @@ import javax.servlet.http.HttpServletRequest;
 //@CrossOrigin(origins = { "*" }, maxAge = 6000)
 public class BoardController {
 
-	private static final Logger logger = LoggerFactory.getLogger(BoardController.class);
-	private static final String SUCCESS = "success";
-	private static final String FAIL = "fail";
+    private static final Logger logger = LoggerFactory.getLogger(BoardController.class);
+    private static final String SUCCESS = "success";
+    private static final String FAIL = "fail";
 
-	@Autowired
-	private BoardService boardService;
+    @Autowired
+    private BoardService boardService;
 
-	@Autowired
-	private JwtService jwtService;
+    @Autowired
+    private BoardCommentService boardCommentService;
+
+    @Autowired
+    private JwtService jwtService;
 
     @ApiOperation(value = "게시판 글목록", notes = "모든 게시글의 정보를 반환한다.", response = List.class)
-	@GetMapping
-	public ResponseEntity<List<Board>> retrieveBoard() throws Exception {
-		logger.debug("retrieveBoard - 호출");
-		return new ResponseEntity<List<Board>>(boardService.retrieveBoard(), HttpStatus.OK);
-	}
+    @GetMapping
+    public ResponseEntity<List<Board>> retrieveBoard() throws Exception {
+        logger.debug("retrieveBoard - 호출");
+        return new ResponseEntity<List<Board>>(boardService.retrieveBoard(), HttpStatus.OK);
+    }
 
-    @ApiOperation(value = "게시판 글보기", notes = "글번호에 해당하는 게시글의 정보를 반환한다.", response = Board.class)    
-	@GetMapping("{articleno}")
-	public ResponseEntity<Board> detailBoard(@PathVariable int articleno) {
-		logger.debug("detailBoard - 호출");
-		boardService.updateHit(articleno);
-		return new ResponseEntity<Board>(boardService.detailBoard(articleno), HttpStatus.OK);
-	}
+    @ApiOperation(value = "게시판 글보기", notes = "글번호에 해당하는 게시글의 정보를 반환한다.", response = Board.class)
+    @GetMapping("{articleno}")
+    public ResponseEntity<Board> detailBoard(@PathVariable int articleno) {
+        logger.debug("detailBoard - 호출");
+        boardService.updateHit(articleno);
+        boardCommentService.SearchBoardCommentsByArticleNo(articleno)
+                           .stream()
+                           .forEach(System.out::println);
+        return new ResponseEntity<Board>(boardService.detailBoard(articleno), HttpStatus.OK);
+    }
 
     @ApiOperation(value = "게시판 글등록", notes = "새로운 게시글 정보를 입력한다. 그리고 DB입력 성공여부에 따라 'success' 또는 'fail' 문자열을 반환한다.", response = String.class)
-	@PostMapping
-	public ResponseEntity<String> writeBoard(@RequestBody Board board) {
-		logger.debug("writeBoard - 호출");
-		if (boardService.writeBoard(board)) {
-			return new ResponseEntity<String>(SUCCESS, HttpStatus.OK);
-		}
-		return new ResponseEntity<String>(FAIL, HttpStatus.NO_CONTENT);
-	}
+    @PostMapping
+    public ResponseEntity<String> writeBoard(@RequestBody Board board) {
+        logger.debug("writeBoard - 호출");
+        if (boardService.writeBoard(board)) {
+            return new ResponseEntity<String>(SUCCESS, HttpStatus.OK);
+        }
+        return new ResponseEntity<String>(FAIL, HttpStatus.OK);
+    }
 
     @ApiOperation(value = "게시판 글정보 수정", notes = "글번호에 해당하는 게시글의 정보를 수정한다. 그리고 DB수정 성공여부에 따라 'success' 또는 'fail' 문자열을 반환한다.", response = String.class)
-	@PutMapping("{articleno}")
-	public ResponseEntity<String> updateBoard(@RequestBody Board board, HttpServletRequest request) throws Exception {
-		logger.debug("updateBoard - 호출");
+    @PutMapping("{articleno}")
+    public ResponseEntity<String> updateBoard(@RequestBody Board board, HttpServletRequest request) throws Exception {
+        logger.debug("updateBoard - 호출");
 
-		String token = request.getHeader("access-token");
+        String token = request.getHeader("access-token");
 
-		logger.debug("token : {}",token);
-		if(!jwtService.isUsable(token)){
-			return new ResponseEntity<String>(FAIL, HttpStatus.UNAUTHORIZED);
-		}
+        logger.debug("token : {}", token);
+        if (!jwtService.isUsable(token)) {
+            return new ResponseEntity<String>(FAIL, HttpStatus.UNAUTHORIZED);
+        }
 
-		String userId = jwtService.getUserIdByToken(token);
+        String userId = jwtService.getUserIdByToken(token);
 
-		logger.debug("userId : {}",userId);
+        logger.debug("userId : {}", userId);
 
-		if (boardService.updateBoard(board,userId)) {
-			return new ResponseEntity<String>(SUCCESS, HttpStatus.OK);
-		}
-		return new ResponseEntity<String>(FAIL, HttpStatus.NO_CONTENT);
-	}
+        if (boardService.updateBoard(board, userId)) {
+            return new ResponseEntity<String>(SUCCESS, HttpStatus.OK);
+        }
+        return new ResponseEntity<String>(FAIL, HttpStatus.OK);
+    }
 
     @ApiOperation(value = "게시판 글삭제", notes = "글번호에 해당하는 게시글의 정보를 삭제한다. 그리고 DB삭제 성공여부에 따라 'success' 또는 'fail' 문자열을 반환한다.", response = String.class)
-	@DeleteMapping("{articleno}")
-	public ResponseEntity<String> deleteBoard(@PathVariable int articleno,  HttpServletRequest request) throws Exception {
-		logger.debug("deleteBoard - 호출");
+    @DeleteMapping("{articleno}")
+    public ResponseEntity<String> deleteBoard(@PathVariable int articleno, HttpServletRequest request) throws Exception {
+        logger.debug("deleteBoard - 호출");
 
-		//발행된 토큰이 존재하지 않는다면
+        //발행된 토큰이 존재하지 않는다면
 
-		String token = request.getHeader("access-token");
+        String token = request.getHeader("access-token");
 
-		logger.debug("token : {}",token);
-		if(!jwtService.isUsable(token)){
-			return new ResponseEntity<String>(FAIL, HttpStatus.UNAUTHORIZED);
-		}
+        logger.debug("token : {}", token);
+        if (!jwtService.isUsable(token)) {
+            return new ResponseEntity<String>(FAIL, HttpStatus.UNAUTHORIZED);
+        }
 
-		String userId = jwtService.getUserIdByToken(token);
+        String userId = jwtService.getUserIdByToken(token);
 
-		logger.debug("userId : {}",userId);
-		if (boardService.deleteBoard(articleno,userId)) {
+        logger.debug("userId : {}", userId);
+        if (boardService.deleteBoard(articleno, userId)) {
+            return new ResponseEntity<String>(SUCCESS, HttpStatus.OK);
+        }
+        return new ResponseEntity<String>(FAIL, HttpStatus.OK);
+    }
+
+	@ApiOperation(value = "게시판 댓글등록", notes = "새로운 게시글 정보를 입력한다. 그리고 DB입력 성공여부에 따라 'success' 또는 'fail' 문자열을 반환한다.", response = String.class)
+	@PostMapping("/comment")
+	public ResponseEntity<String> writeBoardComment(@RequestBody BoardComment boardComment) {
+		logger.debug("writeBoardComment - 호출");
+
+
+		if (boardCommentService.writeBoardComment(boardComment)) {
 			return new ResponseEntity<String>(SUCCESS, HttpStatus.OK);
 		}
 		return new ResponseEntity<String>(FAIL, HttpStatus.OK);
+
 	}
 }
